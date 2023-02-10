c7a8bdfe7756ff7a8265c8a9f32280a78f7b5de7
skjsjhb <113125446+skjsjhb@users.noreply.github.com>
2023-02-10 19:17:58 +0800

    feat(*): add mods

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+self.addEventListener('install', function() {});
+self.addEventListener('fetch', (event) => {
+	/**@type {Request} */
+	const request = event.request;
+	// Let the browser do its default thing
+	// for non-GET requests.
+	if (request.method !== 'GET') return;
+	if (request.url.includes('music.163.com')) return;
+	// Prevent the default, and handle the request ourselves.
+	event.respondWith(
+		(async () => {
+			// Try to get the response from a cache.
+			const cache = await caches.open('qwq');
+			const cachedResponse = await cache.match(request);
+			if (cachedResponse) {
+				// If we found a match in the cache, return it, but also
+				// update the entry in the cache in the background.
+				event.waitUntil(cache.add(request));
+				return cachedResponse;
+			} else {
+				const reg = /data|jsdelivr|baomitu|googleapis|loli/;
+				// If we didn't find a match in the cache, use the network.
+				const response = await fetch(request);
+				if (response.ok && reg.test(request.url)) event.waitUntil(cache.put(request, response.clone()));
+				return response;
+			}
+		})());
+});
